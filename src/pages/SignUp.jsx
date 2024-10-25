394d16fa0917640ee5c41e994c49e54b069deff1
kasumi198 <kasumi3099@gmail.com>
2024-10-25 20:51:06 +0900

    tuika

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,39 @@
 function SignUp() {
-  return <h2>SignUp Page</h2>
+  return (
+    <div>
+      <h1>新規登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>ユーザー名:</label>
+          <input type="user_name" value={user_name} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
+    </div>
+  )
 }
 
 export default SignUp
