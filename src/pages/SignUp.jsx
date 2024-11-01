9408d7057b641db96b291df5b8f976f9adb577e7
kasumi198 <kasumi3099@gmail.com>
2024-11-01 12:06:59 +0900

    SignUpページのスタイルを追加

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import './SignUp.css'; // CSSファイルをインポート
 
 function SignUp() {
   const [user_name, setUser_name] = useState('')
