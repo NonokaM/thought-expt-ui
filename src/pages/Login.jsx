017c8a68859b2f2847978c7e09587db9b917c049
NonokaM <c1201071@gmail.com>
2024-11-04 04:23:05 +0900

    refactor:

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,16 +4,10 @@ import useLogin from './../hooks/useLogin'
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
   const { login, error } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-
-    if (password !== passwordConfirm) {
-      alert('Passwords do not match')
-      return
-    }
 
     login(email, password)
   }
@@ -32,15 +26,6 @@ function Login() {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            value={passwordConfirm}
-            onChange={(e) => setPasswordConfirm(e.target.value)}
             required
           />
         </div>
